f8855ae9fb4f3a1e05de615fa916569b4687070a
hkalinouski <7742589@gmail.com>
2017-02-20 15:37:04 +0300

    subscribe and publish requests

--- server.js
+++ server.js
@@ -2,6 +2,8 @@ const Koa = require('koa');
 const app = new Koa();
 const Router = require('koa-router');
 const router = new Router();
+const url = require('url');
+const querystring = require('querystring');
 let clients = [];
 
 
@@ -10,16 +12,37 @@ router.get('/', async (ctx) => {
 });
 
 router.get('/subscribe', async (ctx) => {
-  await new Promise((resolve, reject) => {
-    clients.push(resolve);
-  })
+  // subribe - подписаться на событие
+  // 1. принять
+  // 2. хранить/запомнить
+  // 3. ответить при появлении события на которое клиент подписан
+  console.log('subribe');
+ let message = await new Promise((resolve, reject) => {
+   clients.push(resolve);
+ });
+
+ ctx.body = message;
 });
 
 router.get('/publish', async (ctx) => {
-  clients.forEach((client) =>{
-      client();
+  // прочитать тело сообщения
+  // подождать завершения чтения
+  // отправить сообщение ожидающим клиентам
+  console.log('publish');
+  let queryUrl = url.parse(ctx.request.url).query;
+  let message = querystring.parse(queryUrl).message;
+ 
+
+  let _emit = ctx.req.emit;
+  ctx.req.emit = function(event, ...args) {
+    console.log(event);
+    _emit.call(ctx.req, event, ...args);
+  };
+  
+  
+  clients.forEach(function(client){
+     client(message);
   });
-  console.log(clients);
 });
 
 app.use(router.routes());
